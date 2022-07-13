5b3a0b8f3f0f3464f394197b4e940801bba49621
ChrisAHeSun <97868669+ChrisAHeSun@users.noreply.github.com>
2022-07-13 09:21:05 +0800

    Upload by PicGo

---
+++ init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();//侧边栏
+    $('.parallax').parallax();
+    $('.collapsible').collapsible();//折叠
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
