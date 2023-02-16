dfac62017b8a5eb19179876f27e90a46fa906dd7
Chris <amiaochris@gmail.com>
2023-02-16 10:48:26 +0800

    Delete all files in branch

--- init.js
+++
@@ -1,9 +0,0 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();//侧边栏
-    $('.parallax').parallax();
-    $('.collapsible').collapsible();//折叠
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
